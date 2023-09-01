ea60a06caa06f8fed880a2613bcff344c104c4e4
Fer <fwfurtado@gmail.com>
2023-08-31 22:33:27 -0300

    Add Rest API (#8)
    * feat: add http entrypoint
    
    * feat: add database

--- Core/Gatekeeper.Core/Entities/Unit.cs
+++ Core/Gatekeeper.Core/Entities/Unit.cs
@@ -4,7 +4,9 @@ namespace Gatekeeper.Core.Entities;
 
 public class Unit
 {
-    private readonly List<Resident> _residents; 
+    public long Id { get; private set; }
+
+    private readonly List<Resident> _residents;
     public string Identifier { get; private set; }
     public IEnumerable<Resident> Residents => _residents.AsReadOnly();
 
@@ -19,9 +21,13 @@ public class Unit
         _residents = new List<Resident>();
     }
 
+    public Unit(long id, string identifier) : this(identifier)
+    {
+        Id = id;
+    }
+
     public void AssociateResident(Resident resident)
     {
         _residents.Add(resident);
     }
-    
 }
---
+++ Database/Gatekeeper.Migration/Migrations/V000001_create-units-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE units
+(
+    id         BIGSERIAL    NOT NULL,
+    identifier VARCHAR(255) NOT NULL,
+
+    CONSTRAINT units_pkey PRIMARY KEY (id)
+
+);
