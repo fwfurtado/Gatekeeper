757f7fa7d130c34912e1af3f3745ab1b8671a0db
Fernando <fwfurtado@gmail.com>
2023-09-12 23:35:51 -0300

    ci: improving sonnar analisys

--- Rest/Gatekeeper.Rest.Test/Configurations/AcceptanceTestFactory.cs
+++ Rest/Gatekeeper.Rest.Test/Configurations/AcceptanceTestFactory.cs
@@ -1,15 +1,7 @@
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Gatekeeper.Rest.Test.Configurations;
 
 public class AcceptanceTestFactory: WebApplicationFactory<Program> 
 {
-    
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        base.ConfigureWebHost(builder);
-        builder.UseEnvironment("AcceptanceTest");
-    }
-    
 }
