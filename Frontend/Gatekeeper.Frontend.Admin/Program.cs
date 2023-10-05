57db517a250e26afb0baa07da1e001e7486694fe
paty <paty.ozono@gmail.com>
2023-10-05 20:52:16 -0300

    feat: add cpf validation

--- Frontend/Gatekeeper.Frontend.Admin/Program.cs
+++ Frontend/Gatekeeper.Frontend.Admin/Program.cs
@@ -2,12 +2,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Gatekeeper.Frontend.Admin;
 using MudBlazor.Services;
+using Gatekeeper.Core.Validations;
+using Gatekeeper.Core.Specifications;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5032") });
+builder.Services.AddScoped<ICpfSpecification, CpfSpecification>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
