846e70c766a388d99073af1538896ef6c3c61ae6
Fernando <fwfurtado@gmail.com>
2023-10-05 21:52:26 -0300

    feat: add a lot of things

--- Frontend/Gatekeeper.Frontend.Admin/Program.cs
+++ Frontend/Gatekeeper.Frontend.Admin/Program.cs
@@ -1,16 +1,18 @@
+using Gatekeeper.Frontend.Admin;
+using Gatekeeper.Frontend.Admin.Services;
+using Gatekeeper.Frontend.Admin.Validations;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Gatekeeper.Frontend.Admin;
 using MudBlazor.Services;
-using Gatekeeper.Core.Validations;
-using Gatekeeper.Core.Specifications;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5032") });
-builder.Services.AddScoped<ICpfSpecification, CpfSpecification>();
+builder.Services.AddScoped<CpfValidator>();
+builder.Services.AddScoped<ResidentFormValidator>();
+builder.Services.AddScoped<ResidentService>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
