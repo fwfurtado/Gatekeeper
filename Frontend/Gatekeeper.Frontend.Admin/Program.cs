bb0408f798e9a97adeec122f492b50384a8fb02e
Fernando <fwfurtado@gmail.com>
2023-10-16 16:35:23 -0300

    feat: add login feature to frontend

--- Frontend/Gatekeeper.Frontend.Admin/Program.cs
+++ Frontend/Gatekeeper.Frontend.Admin/Program.cs
@@ -9,6 +9,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.MetadataUrl = "http://localhost:8080/realms/gatekeeper/.well-known/openid-configuration";
+    options.ProviderOptions.Authority = "http://localhost:8080/realms/gatekeeper";
+    options.ProviderOptions.ClientId = "backoffice";
+    options.ProviderOptions.ResponseType = "code";
+});
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5032") });
 builder.Services.AddScoped<CpfValidator>();
 builder.Services.AddScoped<ResidentFormValidator>();
