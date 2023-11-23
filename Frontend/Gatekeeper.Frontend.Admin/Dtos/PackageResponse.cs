1841946e051b89ea3cfd7f9a8802e8a62a815496
paty <paty.ozono@gmail.com>
2023-11-23 20:51:59 -0300

    feat: add reject and deliver icons

--- Frontend/Gatekeeper.Frontend.Admin/Dtos/PackageResponse.cs
+++ Frontend/Gatekeeper.Frontend.Admin/Dtos/PackageResponse.cs
@@ -7,9 +7,12 @@ namespace Gatekeeper.Frontend.Admin.Dtos;
 public class PackageResponse
 {
     public long Id { get; set; }
-    public string Description { get; set; } = FilterOperator.String.Empty;
+    public string Description { get; set; } = string.Empty;
     [JsonPropertyName("unit_id")] public long UnitId { get; set; }
     [JsonPropertyName("arrived_at")] public DateTime ArrivedAt { get; set; }
     [JsonPropertyName("delivered_at")] public DateTime? DeliveredAt { get; set; }
-    public string Status { get; set; } = FilterOperator.String.Empty;
+    public string Status { get; set; } = string.Empty;
+
+    public bool CanDeliver => Status == "Pending";
+    public bool CanReject => Status == "Pending";
 }
