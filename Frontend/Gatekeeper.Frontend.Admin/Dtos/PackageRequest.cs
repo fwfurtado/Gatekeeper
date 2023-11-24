632fd3e2d599fd14c4c06281e99b478d8dbe6793
paty <paty.ozono@gmail.com>
2023-11-23 21:56:58 -0300

    fix: adjust null to string.Empty

--- Frontend/Gatekeeper.Frontend.Admin/Dtos/PackageRequest.cs
+++ Frontend/Gatekeeper.Frontend.Admin/Dtos/PackageRequest.cs
@@ -4,13 +4,13 @@ namespace Gatekeeper.Frontend.Admin.Dtos;
 
 public class PackageRequest
 {
-    public string Description { get; set; } = null;
+    public string Description { get; set; } = string.Empty;
     public long UnitId { get; set; }
 }
 
 public class PackageForm
 {
-    public string Description { get; set; } = null;
+    public string Description { get; set; } = string.Empty;
     public long UnitId { get; set; }
 }
 
